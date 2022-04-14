c7505ad93f6bebbda1b6a98e801d083416fffbb8
bronco-henry <na.shaddock@gmail.com>
2022-04-13 20:30:20 -0500

    fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dataset import Dataset
 from train import train, predict, eval
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--max_epochs', type=int, default=1)
+parser.add_argument('--max_epochs', type=int, default=11)
 parser.add_argument('--batch_size', type=int, default=256)
 parser.add_argument('--sequence_length', type=int, default=4)
 args = parser.parse_args()
